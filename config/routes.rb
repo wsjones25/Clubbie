1457969fed051a6c0af371aed383edf636be5ec3
Will Jones <wsjones1101@gmail.com>
2018-12-17 13:25:51 +1300

    Create a login helper and indiviual login / sign_up pages

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,8 @@ Rails.application.routes.draw do
   root to:'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'login', to: 'pages#login'
+  get 'sign_up', to: 'pages#sign_up'
+  
   resources :clubs
 end
