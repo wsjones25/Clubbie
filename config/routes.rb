ee51edfa154d30fcd9083b1b09290e17b3373d24
Will Jones <wsjones1101@gmail.com>
2018-12-17 13:16:50 +1300

    Implemented the controllers, model and views for both a user and club_user, and also configured the devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    sessions:           "users/sessions",
+    passwords:          "users/passwords",
+    registrations:      "users/registrations",
+    confirmations:      "users/confirmations",
+    unlocks:            "users/unlocks"
+  }
+
+  devise_for :club_users, controllers: {
+    sessions:           "club_users/sessions",
+    passwords:          "club_users/passwords",
+    registrations:      "club_users/registrations",
+    confirmations:      "club_users/confirmations",
+    unlocks:            "club_users/unlocks"
+  }
+
   root to:'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
